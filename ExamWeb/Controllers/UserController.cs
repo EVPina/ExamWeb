86e55e77790dcce8edd0f21d2f37457d5eeb3d7e
enzo vargas pina <Enzo.VP@outlook.com>
2022-12-29 00:38:57 -0500

    Se agrego configuracion de cuenta

---
+++ ExamWeb/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+      
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+
+        public ActionResult Cuenta()
+        {
+            return View();
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult OptionsProfile(string ValueOpcion)
+        {
+            string opcion = "";
+            switch (ValueOpcion)
+            {
+                case "1":
+                    opcion = "Cuenta";
+                    return Json(new { opcion = opcion });
+                case "2":
+                    opcion = "ChangePassword";
+                    return Json(new { opcion = opcion });
+                default:
+                    opcion = "UserProfile";
+                    return Json(new { opcion = opcion });
+            }
+
+        }
+    }
+}
